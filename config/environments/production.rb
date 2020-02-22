2ea015396f85f1013189cb176060e1e6459d34eb
Leo Midili <leomidili07@gmail.com>
2020-02-21 20:23:44 -0500

    Update ruby to 2.6.0, rails to 5.2.1.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "FunnyComics_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "funny_comics_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
